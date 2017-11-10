a5ae9b9aab219e948cbfbbd4facd4db0e84e2c81
Your Actual Name, In Quotes <Paloma@MacBook-Pro-de-Paloma.local>
2017-11-10 19:45:21 +0100

    Make changes to vikings

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -15,9 +15,18 @@ function Viking (name, health, strength) {
   this.health = health;
   this.strength = strength;
 }
-Viking.prototype = Soldier.prototype;
-// Saxon
-function Saxon () {}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.receiveDamage = function (damage) {
+   this.health = this.health - damage;
+   if (damage <=  this.health ){
+     return this.name + " has received " + damage + " points of damage";
+   } else {
+     return this.name + " has died in act of combat";
+   }
+ };
 
+// Saxon
+function Saxon (health, strength) {}
+Saxon.prototype = Object.create(Soldier.prototype);
 // War
 function War () {}
